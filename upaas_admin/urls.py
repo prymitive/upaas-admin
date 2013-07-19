cb9f1a8a859efbbacd835aa0d58117f75b682c01
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-07-19 22:50:40 +0200

    password change form

--- upaas_admin/urls.py
+++ upaas_admin/urls.py
@@ -26,9 +26,17 @@ v1_api.register(RouterResource())
 urlpatterns = patterns(
     '',
     url(r'^$', 'upaas_admin.apps.site.views.index', name='site_index'),
+
     url(r'^login$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.haml'}),
+        {'template_name': 'users/login.haml'}),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
+
+    url(r'^password$', 'django.contrib.auth.views.password_change',
+        {'template_name': 'users/password.haml'}, name='password'),
+    url(r'^password/changed$',
+        'django.contrib.auth.views.password_change_done',
+        {'template_name': 'users/password_changed.haml'},
+        name='password_changed'),
 
     (r'^api/', include(v1_api.urls)),
 
