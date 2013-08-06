ac7ea175b9654d4b79f9d39b05caf50a18e59674
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-08-06 22:08:14 +0200

    fixed ajax task status for applicationless tasks

--- upaas_admin/apps/tasks/ajax.py
+++ upaas_admin/apps/tasks/ajax.py
@@ -21,12 +21,10 @@ from upaas_admin.apps.tasks.models import Task
 @login_required
 @dajaxice_register
 def task_status(request, task_id):
-    # fetch user application
-    apps = Application.objects(owner=request.user)
-    # filter tasks we can display
-    task = Task.objects.filter(application__in=apps, task_id=task_id).first()
+    task = Task.objects.filter(task_id=task_id).first()
     if task:
-        # if task is owned by user we can display its status
-        result = AsyncResult(task_id)
-        return dumps(dict(status=result.status, result=result.result))
-    raise PermissionDenied
+        if not task.application or task.application.owner == request.user:
+            result = AsyncResult(task_id)
+            return dumps(dict(status=result.status, result=result.result))
+        raise PermissionDenied
+    return dumps({})
