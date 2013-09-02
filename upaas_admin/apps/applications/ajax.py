d23bcdc6572c9dfc207f9e5458e83e2d890afcb7
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-09-02 23:54:15 +0200

    basic actions via web ui (WIP)

---
+++ upaas_admin/apps/applications/ajax.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2013 by Łukasz Mierzwa
+    :contact: l.mierzwa@gmail.com
+"""
+
+
+from json import dumps
+
+from django.contrib.auth.decorators import login_required
+
+from dajaxice.decorators import dajaxice_register
+
+from upaas_admin.apps.applications.models import Application
+
+
+@login_required
+@dajaxice_register
+def build_package(request, app_id):
+    task_id = None
+    app = Application.objects.filter(id=app_id, owner=request.user).first()
+    if app and app.metadata:
+        task_id = app.build_package()
+    return dumps({'task_id': task_id})
+
+
+@login_required
+@dajaxice_register
+def start_app(request, app_id):
+    task_id = None
+    app = Application.objects.filter(id=app_id, owner=request.user).first()
+    if app and app.metadata:
+        task_id = app.start_application()
+    return dumps({'task_id': task_id})
+
+
+@login_required
+@dajaxice_register
+def stop_app(request, app_id):
+    task_id = None
+    app = Application.objects.filter(id=app_id, owner=request.user).first()
+    if app and app.metadata:
+        task_id = app.stop_application()
+    return dumps({'task_id': task_id})
