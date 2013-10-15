d5875a5f3fb2cd2f714373c44f73b39f5474332f
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-10-15 22:43:28 +0200

    remove old discovery

--- upaas_admin/apps/tasks/__init__.py
+++ upaas_admin/apps/tasks/__init__.py
@@ -3,17 +3,3 @@
     :copyright: Copyright 2013 by Łukasz Mierzwa
     :contact: l.mierzwa@gmail.com
 """
-
-
-import logging
-
-from registry import tasks_autodiscover
-
-
-log = logging.getLogger(__name__)
-
-
-try:
-    tasks_autodiscover()
-except:
-    log.debug(u"Task discovery failed")
