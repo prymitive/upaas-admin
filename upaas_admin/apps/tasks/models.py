fee50c9da89c284d80ca5014626607e7fd7d822e
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-10-30 18:59:50 +0100

    move handler to tasks, circular imports

--- upaas_admin/apps/tasks/models.py
+++ upaas_admin/apps/tasks/models.py
@@ -20,10 +20,40 @@ from upaas.processes import is_pid_running
 from upaas_admin.apps.tasks.constants import (
     STATUS_CHOICES, ACTIVE_TASK_STATUSES, TaskStatus)
 from upaas_admin.apps.tasks.registry import find_task_class
-from upaas_admin.common.logger import MongoLogHandler
 
 
 log = logging.getLogger(__name__)
+
+
+class MongoLogHandler(logging.StreamHandler):
+
+    def __init__(self, task, flush_count=10, flush_time=3, *args,
+                 **kwargs):
+        super(MongoLogHandler, self).__init__(*args, **kwargs)
+
+        self.task = task
+        self.flush_count = flush_count
+        self.flush_time = flush_time
+
+        self.messages = []
+        self.last_flush = datetime.datetime.now()
+
+    def flush(self):
+        self.task.__class__.objects(id=self.task.id).update_one(
+            push_all__messages=self.messages)
+        self.messages = []
+        self.last_flush = datetime.datetime.now()
+
+    def emit(self, record):
+        msg = TaskMessage(
+            timestamp=datetime.datetime.fromtimestamp(record.created),
+            source=record.name, level=record.levelno, message=record.msg)
+        self.messages.append(msg)
+        if len(self.messages) >= self.flush_count:
+            self.flush()
+        elif datetime.datetime.now() - self.last_flush >= datetime.timedelta(
+                seconds=self.flush_time):
+            self.flush()
 
 
 class TaskMessage(EmbeddedDocument):
