7184769f44275f900eb23b296bdd6b0772e742f8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-08 21:51:35 +0100

    bump for ppa

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='upaas-admin',
-    version='0.2-dev3',
+    version='0.2-dev4',
     license='GPLv3',
     description="UPaaS admin API and UI",
     author="Łukasz Mierzwa",
