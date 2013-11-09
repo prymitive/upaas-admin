1c6d42492dc3b2d5675dc3b9ca64f31afc3cc342
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-09 21:54:13 +0100

    improved exceptions to be more informative

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='upaas-admin',
-    version='0.2-dev4',
+    version='0.2-dev5',
     license='GPLv3',
     description="UPaaS admin API and UI",
     author="Łukasz Mierzwa",
