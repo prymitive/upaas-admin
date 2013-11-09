f756f8087abc7e66da7e0ff1fd8d101223f4c282
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-09 17:19:10 +0100

    cleanup tooltips

---
+++ upaas_admin/static/upaas/site.js
@@ -0,0 +1,9 @@
+/*
+:copyright: Copyright 2013 by Łukasz Mierzwa
+:contact: l.mierzwa@gmail.com
+*/
+
+
+$(document).ready(function(){
+    $('.upaas-help-tooltip').tooltip({container:'.upaas-content'});
+});
