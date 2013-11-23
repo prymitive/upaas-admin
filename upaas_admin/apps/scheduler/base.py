a81a7d8a7766d264c898ad0f5bbf101569975c3b
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-23 10:39:51 +0100

    another fix for the scheduler [ci skip]

--- upaas_admin/apps/scheduler/base.py
+++ upaas_admin/apps/scheduler/base.py
@@ -100,7 +100,7 @@ def select_best_backends(run_plan):
 
     backends = []
     for workers_count in workers_per_backend:
-        backend = select_best_backend(exclude=[b.backend.id for b in backends],
+        backend = select_best_backend(exclude=[b.backend for b in backends],
                                       application=run_plan.application)
         if backend:
             ports = backend.find_free_ports(2)
