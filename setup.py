47624644d02a239d5a47158e03c9213be4cdf05b
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-12-21 23:30:50 +0100

    0.2 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='upaas-admin',
-    version='0.2-dev',
+    version='0.2',
     license='GPLv3',
     description="UPaaS admin API and UI",
     author="Łukasz Mierzwa",
